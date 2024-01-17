aa51017cf55562d0e4fa7308caca10a623a7cb5c
Nazarena Rueda <50343570+Naza26@users.noreply.github.com>
2024-01-16 21:04:35 -0300

    add first test

--- TusLibros/tests/domain/tests.py
+++ TusLibros/tests/domain/tests.py
@@ -1,10 +1,9 @@
 import unittest
 
-
 from business.cart import Cart
 
 
-class DomainTests(unittest.TestCase):
+class DomainTest(unittest.TestCase):
     def test_a_cart_with_no_books_is_empty(self):
         cart = Cart()
         self.assertTrue(cart.is_empty())
