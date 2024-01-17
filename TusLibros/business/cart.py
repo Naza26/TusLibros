a1ef0aa9262c519c8f2b63197760a8bd275e8c9e
Nazarena Rueda <50343570+Naza26@users.noreply.github.com>
2024-01-16 21:09:17 -0300

    wip

--- TusLibros/business/cart.py
+++ TusLibros/business/cart.py
@@ -4,3 +4,9 @@ class Cart:
 
     def is_empty(self):
         return len(self.books) == 0
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def contains_book(self, book):
+        return book in self.books
--- TusLibros/tests/domain/tests.py
+++ TusLibros/tests/domain/tests.py
@@ -6,7 +6,22 @@ from business.cart import Cart
 class DomainTest(unittest.TestCase):
     def test_a_cart_with_no_books_is_empty(self):
         cart = Cart()
+
         self.assertTrue(cart.is_empty())
+
+    def test_a_cart_with_books_is_not_empty(self):
+        cart = Cart()
+
+        cart.add_book('Modern Software Engineering')
+
+        self.assertFalse(cart.is_empty())
+
+    def test_the_cart_contains_the_added_book(self):
+        cart = Cart()
+
+        cart.add_book('Modern Software Engineering')
+
+        self.assertTrue(cart.contains_book('Modern Software Engineering'))
 
 
 if __name__ == '__main__':
