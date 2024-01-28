08faa84ffd3a7010203611105781033dff2e4ac1
Nazarena Rueda <50343570+Naza26@users.noreply.github.com>
2024-01-28 19:18:39 -0300

    wip

--- TusLibros/system/internal_system.py
+++ TusLibros/system/internal_system.py
@@ -4,16 +4,9 @@ from business.cart import Cart
 class InternalCartSystem:
     def __init__(self):
         self.cart_system = {}
-        #{'987': {
-        #    'cart': Cart(),
-        #    'client_info': {
-        #        'client_id': '123',
-        #        'password': 'password'
-        #    }
-        #}}
 
     def create_cart(self, client_id, password):
-        self.validate_cart_creation_parameters(client_id, password)
+        self._validate_cart_creation_parameters(client_id, password)
 
         cart = Cart()
         cart_id = self._cart_id()
@@ -21,11 +14,25 @@ class InternalCartSystem:
 
         return cart_id
 
+    def add_to_cart(self, cart_id, book_isbn, book_quantity):
+        self._validate_cart_addition_parameters(cart_id, book_isbn, book_quantity)
+
+        cart = self.cart_system[cart_id]['cart']
+        cart.add_book(book_isbn, book_quantity)
+
     def _cart_id(self):
         return len(self.cart_system) + 1
 
-    def validate_cart_creation_parameters(self, client_id, password):
+    def _validate_cart_creation_parameters(self, client_id, password):
         if client_id is None:
             raise ValueError('Client ID is missing')
         if password is None:
             raise ValueError('Password is missing')
+
+    def _validate_cart_addition_parameters(self, cart_id, book_isbn, book_quantity):
+        if cart_id is None:
+            raise ValueError('Cart ID is missing')
+        if book_isbn is None:
+            raise ValueError('Book ISBN is missing')
+        if book_quantity is None:
+            raise ValueError('Book quantity is missing')
--- TusLibros/tests/system/internal_system_tests.py
+++ TusLibros/tests/system/internal_system_tests.py
@@ -9,6 +9,7 @@ class InternalTests(unittest.TestCase):
         self.system = InternalCartSystem()
         self.client_id = self._a_client_id()
         self.password = self._a_password()
+        self.book_isbn = self._a_book_isbn()
 
     def test_01_cannot_create_cart_when_client_id_is_missing(self):
         client_id = None
@@ -32,12 +33,21 @@ class InternalTests(unittest.TestCase):
 
         self.assertIsNotNone(response)
 
+    def test_03_cannot_add_to_cart_if_client_id_is_missing(self):
+        client_id = None
+
+        try:
+            _ = self.system.add_to_cart(client_id, self.password, self.book_isbn)
+        except ValueError as exception:
+            self.assertEqual(str(exception), 'Client ID is missing')
+
     def _a_client_id(self):
         return 'client_id'
 
     def _a_password(self):
-        password = 'password'
-        return password
+        return 'password'
+    def _a_book_isbn(self):
+        return 'book_isbn'
 
 
 
