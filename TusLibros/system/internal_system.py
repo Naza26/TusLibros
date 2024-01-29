cb305988593373c0655663a99e8749faf51c4b40
Nazarena Rueda <naza120@gmail.com>
2024-01-29 10:58:57 -0300

    wip

--- TusLibros/system/internal_system.py
+++ TusLibros/system/internal_system.py
@@ -17,8 +17,11 @@ class InternalCartSystem:
     def add_to_cart(self, cart_id, book_isbn, book_quantity):
         self._validate_cart_addition_parameters(cart_id, book_isbn, book_quantity)
 
-        cart = self.cart_system[cart_id]['cart']
+        cart = self._cart_with(cart_id)
         cart.add_book(book_isbn, book_quantity)
+
+    def _cart_with(self, cart_id):
+        return self.cart_system[cart_id]['cart']
 
     def _cart_id(self):
         return len(self.cart_system) + 1
--- TusLibros/tests/system/internal_system_tests.py
+++ TusLibros/tests/system/internal_system_tests.py
@@ -14,15 +14,14 @@ class InternalTests(unittest.TestCase):
     def test_01_cannot_create_cart_when_client_id_is_missing(self):
         client_id = None
 
-        self._assert_client_id_is_required_for_cart_creation(client_id)
+        self._assert_parameter_is_required_for_cart_creation(client_id, self.password, "Client ID")
 
     def test_02_cannot_create_cart_when_password_is_missing(self):
         password = None
 
-        self._assert_password_is_required_for_cart_creation(password)
+        self._assert_parameter_is_required_for_cart_creation(self.client_id, password, "Password")
 
     def test_03_can_create_cart_when_client_id_and_password_are_provided(self):
-
         response = self.system.create_cart(self.client_id, self.password)
 
         self.assertIsNotNone(response)
@@ -30,56 +29,38 @@ class InternalTests(unittest.TestCase):
     def test_04_cannot_add_to_cart_if_cart_id_is_missing(self):
         cart_id = None
 
-        self._assert_cart_id_is_required_for_cart_addition(cart_id)
+        self._assert_parameter_is_required_for_cart_addition(cart_id, self.book_isbn, self._a_quantity_of_books(),
+                                                             "Cart ID")
 
     def test_05_cannot_add_to_cart_if_client_id_is_missing(self):
         book_isbn = None
 
-        self._assert_book_isbn_is_required_for_cart_addition(book_isbn)
+        self._assert_parameter_is_required_for_cart_addition(self._a_cart_id(), book_isbn, self._a_quantity_of_books(),
+                                                             "Book ISBN")
 
     def test_06_cannot_add_to_cart_if_book_quantity_is_missing(self):
         book_quantity = None
 
-        self._assert_book_quantity_is_required_for_cart_addition(book_quantity)
+        self._assert_parameter_is_required_for_cart_addition(self._a_cart_id(), self.book_isbn, book_quantity,
+                                                             "Book Quantity")
 
-    def test_07_can_add_to_cart_if_cart_id_and_book_isbn_and_book_quantity_are_provided(self):
-        # test_07_can_add_to_cart_if_required_parameters_are_provided?
-
+    def test_07_can_add_to_cart_if_all_required_parameters_are_provided(self):
         cart_id = self._a_cart_id()
 
         self.system.add_to_cart(cart_id, self.book_isbn, self._a_quantity_of_books())
 
         self._assert_cart_contains_added_book(cart_id)
 
-    def _assert_client_id_is_required_for_cart_creation(self, client_id):
-        try:
-            _ = self.system.create_cart(client_id, self.password)
-        except ValueError as exception:
-            self.assertEqual(str(exception), 'Client ID is missing')
+    def _assert_parameter_is_required_for_cart_creation(self, client_id, password, parameter_name_to_validate):
+        with self.assertRaises(ValueError) as context:
+            self.system.create_cart(client_id, password)
+        self.assertEqual(str(context.exception), f'{parameter_name_to_validate} is missing')
 
-    def _assert_password_is_required_for_cart_creation(self, password):
-        try:
-            _ = self.system.create_cart(self.client_id, password)
-        except ValueError as exception:
-            self.assertEqual(str(exception), 'Password is missing')
-
-    def _assert_cart_id_is_required_for_cart_addition(self, cart_id):
-        try:
-            _ = self.system.add_to_cart(cart_id, self.book_isbn, self._a_quantity_of_books())
-        except ValueError as exception:
-            self.assertEqual(str(exception), 'Cart ID is missing')
-
-    def _assert_book_isbn_is_required_for_cart_addition(self, book_isbn):
-        try:
-            _ = self.system.add_to_cart(self._a_cart_id(), book_isbn, self._a_quantity_of_books())
-        except ValueError as exception:
-            self.assertEqual(str(exception), 'Book ISBN is missing')
-
-    def _assert_book_quantity_is_required_for_cart_addition(self, book_quantity):
-        try:
-            _ = self.system.add_to_cart(self._a_cart_id(), self.book_isbn, book_quantity)
-        except ValueError as exception:
-            self.assertEqual(str(exception), 'Book Quantity is missing')
+    def _assert_parameter_is_required_for_cart_addition(self, cart_id, book_isbn, quantity_of_books,
+                                                        parameter_name_to_validate):
+        with self.assertRaises(ValueError) as context:
+            self.system.add_to_cart(cart_id, book_isbn, quantity_of_books)
+        self.assertEqual(str(context.exception), f'{parameter_name_to_validate} is missing')
 
     def _assert_cart_contains_added_book(self, cart_id):
         self.assertTrue(self._a_cart_with(cart_id).contains_book(self.book_isbn))
