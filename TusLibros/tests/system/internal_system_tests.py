6e2cefe10b0aa0b9f25d674e90c340519ec47cde
Nazarena Rueda <50343570+Naza26@users.noreply.github.com>
2024-02-07 20:17:02 -0300

    wip

--- TusLibros/tests/system/internal_system_tests.py
+++ TusLibros/tests/system/internal_system_tests.py
@@ -3,28 +3,36 @@ import unittest
 from system.internal_system import InternalCartSystem
 
 
+# TODO: Doubt: Is it correct to save the call to some business logic in a variable named result instead of response?
+# I was thinking in the internal face to call it result and in the outer face to call it response
+# HTTP difference? The decision I took seems correct to me but i don't know why yet,
 class InternalTests(unittest.TestCase):
 
     def setUp(self):
         self.system = InternalCartSystem()
 
-    def test_01_can_add_to_cart_if_all_required_parameters_are_provided(self):
+    def test_01_can_create_cart_when_client_id_and_password_are_provided(self):
+        result = self.system.create_cart(self._a_client_id(), self._a_password())
+
+        self._assert_is_valid_cart_id(result)
+
+    def test_02_can_add_to_cart_if_all_required_parameters_are_provided(self):
         cart_id = self.system.create_cart(self._a_client_id(), self._a_password())
 
-        response = self.system.add_to_cart(cart_id, self._a_book_isbn(), self._a_quantity_of_books())
+        result = self.system.add_to_cart(cart_id, self._a_book_isbn(), self._a_quantity_of_books())
 
-        self._assert_books_were_successfully_added_to_cart(response, self._a_book_isbn())
+        self._assert_books_were_successfully_added_to_cart(result, self._a_book_isbn())
 
-    def test_02_can_list_cart_if_all_required_parameters_are_provided(self):
+    def test_03_can_list_cart_if_all_required_parameters_are_provided(self):
         cart_id = self.system.create_cart(self._a_client_id(), self._a_password())
         self.system.add_to_cart(cart_id, self._a_book_isbn(), self._a_quantity_of_books())
 
-        response = self.system.list_cart(self._a_client_id())
+        result = self.system.list_cart(self._a_client_id())
 
-        self._assert_listed_cart_contains_book_isbn_and_quantity(response)
+        self._assert_listed_cart_contains_book_isbn_and_quantity(result)
 
-    def test_03_cannot_add_books_when_cart_does_not_exist(self):
-        non_existing_client_id = self._a_cart_id()
+    def test_04_cannot_add_books_when_cart_does_not_exist(self):
+        non_existing_client_id = self._an_invalid_cart_id()
 
         self._assert_cannot_add_books_to_non_existing_cart(non_existing_client_id)
 
@@ -54,8 +62,14 @@ class InternalTests(unittest.TestCase):
     def _a_quantity_of_books(self):
         return 1
 
-    def _a_cart_id(self):
+    def _an_invalid_cart_id(self):
         return 'cart_id'
+
+    def _assert_is_valid_cart_id(self, result):
+        try:
+            int(result)
+        except ValueError:
+            self.fail("Cart ID is not valid")
 
 
 if __name__ == '__main__':
