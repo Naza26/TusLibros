360402dc6c8395801f2c4f66dd1cb465609e5a1b
Nazarena Rueda <50343570+Naza26@users.noreply.github.com>
2024-03-17 12:20:07 -0300

    wip

--- TusLibros/web_server/web_server.py
+++ TusLibros/web_server/web_server.py
@@ -8,26 +8,34 @@ class WebServer:
         self._port_number = self.DEFAULT_PORT_NUMBER
         self._app = Flask(__name__)
 
-        self.register_routes()
-
-    def application(self):
-        return self._app
+        self._register_endpoints()
 
     def run_with_configuration(self, host, debug, port=DEFAULT_PORT_NUMBER):
         self._app.run(host=host, port=port, debug=debug)
 
-    def register_routes(self):
-        @self._app.route('/')
-        def index():
-            return 'Index Page'
-
-        @self._app.route('/hello')
-        def hello():
-            return 'Hello, World'
-
+    def _register_endpoints(self):
         @self._app.route('/createCart')
         def create_cart():
-            return 'Hello, CART'
+            return self._execute_rest_interface_for('create_cart')
+
+        @self._app.route('/addToCart')
+        def add_to_cart():
+            return self._execute_rest_interface_for('add_to_cart')
+
+        @self._app.route('/listCart')
+        def list_cart():
+            return self._execute_rest_interface_for('list_cart')
+
+        @self._app.route('/checkOutCart')
+        def checkout_cart():
+            return self._execute_rest_interface_for('checkout_cart')
+
+        @self._app.route('/listPurchases')
+        def list_purchases():
+            return self._execute_rest_interface_for('list_purchases')
+
+    def _execute_rest_interface_for(self, endpoint):
+        pass
 
 
 web_server = WebServer()
